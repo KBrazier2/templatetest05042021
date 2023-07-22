c4f6f676ed75fda8d6ce6462a5ac1912036fb71e
Matthew Rikard <66761313+madhatter2084@users.noreply.github.com>
2023-07-22 13:39:44 -0400

    Update FeaturesTiles.js
    undoing previous commit

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -131,23 +131,23 @@ const FeaturesTiles = ({
               </div>
             </div>
 
-            // <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
-            //   <div className="tiles-item-inner">
-            //     <div className="features-tiles-item-header">
-            //       <div className="mb-16">
-            //         <CaretUp color={"#bebebe"} size={64} />
-            //       </div>
-            //     </div>
-            //     <div className="features-tiles-item-content">
-            //       <h4 className="mt-0 mb-8">
-            //         Roofing/Siding
-            //         </h4>
-            //       <p className="m-0 text-sm">
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="mb-16">
+                    <CaretUp color={"#bebebe"} size={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Roofing/Siding
+                    </h4>
+                  <p className="m-0 text-sm">
                     
-            //         </p>
-            //     </div>
-            //   </div>
-            // </div>
+                    </p>
+                </div>
+              </div>
+            </div>
 
             <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
               <div className="tiles-item-inner">
